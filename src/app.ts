61b1488f56df13f78b2f3e25b759fbbb3596a605
izake <akha3743@gmail.com>
2025-04-22 20:06:36 +0530

    add 'upade', 'delete','view' route for updation, deletion,and viewing of single entry

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import http from "node:http";
 import { db } from "./db";
 import { coursesTable, instructorTable, subjectTable } from "./db/schema";
+import { eq } from "drizzle-orm";
 require("dotenv").config();
 
 const PORT = 3000;
@@ -15,18 +16,33 @@ const server = http.createServer(function reqHandler(req, res) {
   req.on("end", async () => {
     const bodyObj = new URLSearchParams(body);
 
+    let ID;
+    try {
+      ID = typeof Number(req.url?.split("/").at(-1))
+        ? Number(req.url?.split("/").at(-1))
+        : null;
+    } catch (e) {
+      console.error(e);
+    }
+
+    console.log("id", ID);
+
     if (req.method === "GET") {
       switch (req.url) {
         case "/":
           res.end(`
-            <a href="/courses">add Course</a>
-            <a href="/subjects">add Subject</a>
-            <a href="/instructors">add instructor</a> 
+            <a href="/addcourse">add Course</a>
+            <a href="/addsubject">add Subject</a>
+            <a href="/addinstructor">add instructor</a>  
+            <br>
+            <a href="/courses/all">get all courses</a>
+            <br>
+            
             `);
           break;
 
-        case "/courses":
-          res.end(`<form action="/courses/all" method="POST">
+        case "/addcourse":
+          res.end(`<form action="/courses" method="POST">
             <input type='text' name="course_name" placeholder='Enter course name...'/>
             <input type='number' name="course_price" placeholder='Enter course price...'/>
             <input type='text' name="course_description" placeholder='Enter course description...'/>
@@ -34,8 +50,8 @@ const server = http.createServer(function reqHandler(req, res) {
             </form>`);
           break;
 
-        case "/instructors":
-          res.end(`<form action="/instructors/all" method="POST">
+        case "/addinstructor":
+          res.end(`<form action="/instructor" method="POST">
             <input type='text' name="teacher_name" placeholder='Enter teacher name...'/>
             <input type='email' name="teacher_email" placeholder='Enter teacher email...'/>
             <input type='text' name="teacher_description" placeholder='Enter teacher descption...'/>
@@ -43,12 +59,109 @@ const server = http.createServer(function reqHandler(req, res) {
             </form>`);
           break;
 
-        case "/subjects":
-          res.end(`<form action="/subjects/all" method="POST">
+        case "/addsubject":
+          res.end(`<form action="/subject" method="POST">
             <input type='text' name="subject_name" placeholder='Enter subject name...'/>
             <input type='text' name="subject_description" placeholder='Enter subject descption...'/>
             <button type="submit">submit</button>
             </form>`);
+          break;
+
+        case `/delete/${ID}`:
+          if (ID !== null) {
+            await db
+              .delete(coursesTable)
+              .where(eq(coursesTable.courseId, Number(ID)));
+
+            res.end(`<h2>course deleted</h2>
+              <br>
+              <a href="/courses/all">see all courses</a>
+              `);
+          } else {
+            res.end(`<h1> invalid id passed at: /delete/${ID}`);
+          }
+          break;
+
+        case `/update/${ID}`:
+          const course = await db
+            .select()
+            .from(coursesTable)
+            .where(eq(coursesTable.courseId, Number(ID)));
+
+          res.end(`<form action="/update/${ID}" method="POST">
+            <p>current course name:<b> ${course[0].courseName} </b> </p>
+            <input type='text' name="course_name" placeholder='Enter course name...'/>
+            <p>current course price:<b> ${course[0].coursePrice} </b></p>
+            <input type='number' name="course_price"  placeholder='Enter course price...'/>
+            <p>current coures description:<b> ${course[0].courseDescription} </b></p>
+              <input type='text' name="course_description"  placeholder='Enter course description...'/>
+              <button  action="/update/${ID}" type="submit" >change data</button>
+              </form>`);
+          break;
+
+        case `/view/${ID}`:
+          const currentCourse = await db
+            .select()
+            .from(coursesTable)
+            .where(eq(coursesTable.courseId, Number(ID)));
+
+          let c = `<h1> course </h1> 
+            <table border="2">
+            <tr>
+            <th>course name</th>
+            <th>course price</th>
+            <th>course desc.</th>
+            <tr>`;
+          currentCourse.forEach((course) => {
+            c += `
+                <tr>
+                <td>${course.courseName}</td>
+                <td>${course.coursePrice}</td>
+                <td>${course.courseDescription}</td>
+                </tr>
+                `;
+          });
+          c += `</table> <br><a href="/">Back to Home</a>`;
+          res.end(c);
+          break;
+
+        case "/courses/all":
+          try {
+            const allCourses = await db.select().from(coursesTable);
+
+            let html = `<h1> All courses </h1> 
+            <table border="2">
+            <tr>
+            <th>course name</th>
+            <th>course price</th>
+            <th>course desc.</th>
+
+            <th> delete course</th>
+            <th> edit course</th>
+            <th> view course</th>
+
+            <tr>
+            `;
+
+            allCourses.forEach((course) => {
+              html += `
+                    <tr>
+                    <td>${course.courseName}</td>
+                    <td>${course.coursePrice}</td>
+                    <td>${course.courseDescription}</td>
+                    <td><a href="/delete/${course.courseId}">Delete course</a></td>
+                    <td><a href="/update/${course.courseId}">Edit course</a></td>
+                    <td><a href="/view/${course.courseId}">view Course</a></td>
+                    </tr>
+
+                    `;
+            });
+            html += `</table> <br><a href="/">Back to Home</a>`;
+            res.end(html);
+          } catch (err) {
+            res.statusCode = 500;
+            res.end("<h2>Internal Server Error</h2>");
+          }
           break;
 
         default:
@@ -57,57 +170,87 @@ const server = http.createServer(function reqHandler(req, res) {
           break;
       }
     } else if (req.method === "POST") {
-    }
+      switch (req.url) {
+        case "/courses":
+          const course_name = bodyObj.get("course_name");
+          const course_price = bodyObj.get("course_price");
+          const course_description = bodyObj.get("course_description");
 
-    //=======================================================
-
-    if (req.method === "POST" && req.url === "/courses/all") {
-      const course_name = bodyObj.get("course_name");
-      const course_price = bodyObj.get("course_price");
-      const course_description = bodyObj.get("course_description");
-
-      if (course_name && course_price && course_description) {
-        await db.insert(coursesTable).values({
-          courseName: course_name,
-          coursePrice: parseInt(course_price),
-          courseDescription: course_description,
-        });
-        res.end(`
+          if (course_name && course_price && course_description) {
+            await db.insert(coursesTable).values({
+              courseName: course_name,
+              coursePrice: parseInt(course_price),
+              courseDescription: course_description,
+            });
+            res.end(`
           <p style ="background-color: lightseagreen; color: red; font-family: monospace">Course Name: ${course_name}</p>
           <p>Course price: ${course_price}</p>
           <p>Course description: ${course_description}</p>
-          <a href="/course">Create another course</a>
+          <a href="/addcourse">Create another course</a><br/>
+          <a href="/courses/all">get All courses</a>
         `);
-      }
-    }
-    //=======================================================
+          }
+          break;
 
-    if (req.method === "POST" && req.url === "/instructors/all") {
-      const teacher_name = bodyObj.get("teacher_name");
-      const teacher_email = bodyObj.get("teacher_email");
-      const teacher_description = bodyObj.get("teacher_description");
+        case "/instructor":
+          const teacher_name = bodyObj.get("teacher_name");
+          const teacher_email = bodyObj.get("teacher_email");
+          const teacher_description = bodyObj.get("teacher_description");
 
-      if (teacher_name && teacher_email && teacher_description) {
-        await db.insert(instructorTable).values({
-          instructorName: teacher_name,
-          instructorEmail: teacher_email,
-          instructorDescription: teacher_description,
-        });
+          if (teacher_name && teacher_email && teacher_description) {
+            await db.insert(instructorTable).values({
+              instructorName: teacher_name,
+              instructorEmail: teacher_email,
+              instructorDescription: teacher_description,
+            });
+            res.end(`
+              <p style ="background-color: lightseagreen; color: red; font-family: monospace">teacher Name: ${teacher_name}</p>
+              <p>teacher Email: ${teacher_email}</p>
+              <p>teacher description: ${teacher_description}</p>
+              <a href="/addinstructor">Create another teachers</a><br/>
+              <a href="/teachers/all">get All teachers</a>
+            `);
+          }
+          break;
 
-      }
-    }
-    //=======================================================
+        case "/subject":
+          const subject_name = bodyObj.get("subject_name");
+          const subject_description = bodyObj.get("subject_description");
 
-    if (req.method === "POST" && req.url === "/subjects/all") {
-      const subject_name = bodyObj.get("subject_name");
-      const subject_description = bodyObj.get("subject_description");
+          if (subject_name && subject_description) {
+            await db.insert(subjectTable).values({
+              subName: subject_name,
+              subDescription: subject_description,
+            });
+            res.end(`
+              <p style ="background-color: lightseagreen; color: red; font-family: monospace">subject Name: ${subject_name}</p>
+              <p>subject description: ${subject_description}</p>
+              <a href="/addsubject">Create another subject</a><br/>
+              <a href="/subjects/all">get All subjects</a>
+            `);
+          }
+          break;
 
-      if (subject_name && subject_description) {
-        await db.insert(subjectTable).values({
-          subName: subject_name,
-          subDescription: subject_description,
-        });
+        case `/update/${ID}`:
+          const updated_course_name = bodyObj.get("course_name");
+          const updated_course_price = bodyObj.get("course_price");
+          const updated_course_description = bodyObj.get("course_description");
 
+          await db
+            .update(coursesTable)
+            .set({
+              courseName: String(updated_course_name),
+              coursePrice: Number(updated_course_price),
+              courseDescription: String(updated_course_description),
+            })
+            .where(eq(coursesTable.courseId, Number(ID)));
+
+          res.end(
+            `<h1> the data is updated</h1>
+              <br>
+              <a href="/courses/all">get All courses</a>`
+          );
+          break;
       }
     }
   });
